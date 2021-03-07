b9b64601acd4aca6161bd0b0ceb307deed0654f0
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-03-07 13:44:38 -0300

    Retrieves people profile data

--- src/controllers/PeopleController.js
+++ src/controllers/PeopleController.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require("http-status-codes");
-const { readTable, findRegister } = require("../database/interface/read");
+const { findRegister } = require("../database/interface/read");
 const { createRegister } = require("../database/interface/create");
 const ErrorMessage = require("./utils/errorMessages");
 const { updateRegisterWithID } = require("../database/interface/update");
@@ -7,9 +7,36 @@ const table = "people";
 
 module.exports = {
   async read(req, res) {
-    readTable(table).then((dbResponse) => {
-      return res.status(StatusCodes.OK).json(dbResponse);
-    });
+    try {
+      const { username } = req.query;
+      const peopleData = await findRegister(table, "username", username);
+
+      if (!peopleData) throw new Error(ErrorMessage.dataOutOfRange);
+
+      const {
+        id,
+        full_name,
+        birthday,
+        mothers_full_name,
+        email,
+        whatsapp,
+      } = peopleData;
+
+      return res.status(StatusCodes.OK).json({
+        id,
+        full_name,
+        username,
+        birthday,
+        mothers_full_name,
+        email,
+        whatsapp,
+      });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, message: error.message });
+    }
   },
   async findByID_REST(req, res) {
     const { id } = req.params;
